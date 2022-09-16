8ee32772caf8b257379c0a63a96fad1164dac039
bigodom <pedro_augusto50@hotmail.com>
2022-09-16 18:59:21 -0300

    Funcionalidades básicas
    exibe na pagina principal '/' uma lista com os trabalhadores cadastrados e cadastra os trabalhadores na pagina '/cadastro'

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 from mimetypes import init
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 class Cadastro:
-    def __init__(self, nome, bairro, telefone):
+    def __init__(self, nome, trabalho, cidade, bairro, telefone):
         self.nome = nome
+        self.trabalho = trabalho
+        self.cidade = cidade
         self.bairro = bairro
         self.telefone = telefone
 
-cadastro1 = Cadastro('Pedro', 'industrial', '31912341234')
+cadastro1 = Cadastro('Pedro', 'pedreiro', 'João Monlevade', 'industrial', '31912341234')
 lista = [cadastro1]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', titulo='Cadastrados', cadastrados=lista)
 
-@app.route('/registro')
+@app.route('/cadastro')
 def registro():
-    return render_template('registro.html', titulo='Registro')
+    return render_template('registro.html', titulo='Cadastro')
 
-@app.route('/criar')
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
+    trabalho = request.form['trabalho']
+    cidade = request.form['cidade']
     bairro = request.form['bairro']
     telefone = request.form['telefone']
 
-    cadastro = Cadastro(nome, bairro, telefone)
-    lista.append()
+    cadastro = Cadastro(nome, trabalho, cidade, bairro, telefone)
+    lista.append(cadastro)
+    return redirect('/')
 
 app.run()
