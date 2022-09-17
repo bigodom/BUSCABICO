31e872dcb4c0991275d5c1e08144c454b7d5e452
bigodom <pedro_augusto50@hotmail.com>
2022-09-17 11:44:46 -0300

    sistema de login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
+
 
 class Cadastro:
     def __init__(self, nome, trabalho, cidade, bairro, telefone):
@@ -8,20 +9,25 @@ class Cadastro:
         self.bairro = bairro
         self.telefone = telefone
 
+
 cadastro1 = Cadastro('Pedro', 'pedreiro', 'João Monlevade', 'industrial', '31912341234')
 lista = [cadastro1]
 
 app = Flask(__name__)
+app.secret_key = 'malvadao'
+
 
 @app.route('/')
 def index():
     return render_template('index.html', titulo='Cadastrados', cadastrados=lista)
 
+
 @app.route('/cadastro')
 def registro():
     return render_template('registro.html', titulo='Cadastro')
 
-@app.route('/criar', methods=['POST',])
+
+@app.route('/criar', methods=['POST', ])
 def criar():
     nome = request.form['nome']
     trabalho = request.form['trabalho']
@@ -33,4 +39,21 @@ def criar():
     lista.append(cadastro)
     return redirect('/')
 
-app.run()
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if '123' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não reconhecido')
+        return redirect('/login')
+
+
+app.run(debug=True)
